a32c3ebd1e519b956905ef00e29b21c6321ca0ff
Gautam Bhushan <gautambhushan5@gmail.com>
2025-12-25 23:03:46 +0530

    Update Nodemailer configuration for Gmail

--- controller/auth.js
+++ controller/auth.js
@@ -9,12 +9,16 @@ const User = require("../model/user");
 // ========================= Nodemailer Transporter =========================
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10_000,
 });
+
 
 // ========================= GET LOGIN =========================
 
